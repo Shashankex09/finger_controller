d0722cab4064a5b64d5334192fe7cf963d471229
Shashankparmar09 <Shashankparmar62@gmail.com>
2026-04-08 14:27:12 +0530

    Remove unauthorized project files - keeping only commands_by_fingers

--- check_mediapipe.py
+++
@@ -1,4 +0,0 @@
-import mediapipe as mp
-import importlib.util
-print('version:', mp.__version__)
-print('has mediapipe.tasks.python:', importlib.util.find_spec('mediapipe.tasks.python') is not None)
